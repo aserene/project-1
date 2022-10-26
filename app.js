cdcf0f3d1ddebf7aeb4c3bd4a8cfd2754e280796
Athena Serene <aserenehoosang@gmail.com>
2022-10-26 16:37:39 -0700

    Initial HTML Setup

--- app.js
+++ app.js
@@ -3,3 +3,9 @@
 // access token: lpW7spZ1knrCfM4NsWxxPhXKG2oU8FdGctTx4T46yUE
 
 //spaces/{space_id}/environments/{environment_id}/entries?access_token={access_token}&content_type={content_type}
+
+const URL  = "https://cdn.contentful.com/spaces/j5z000zxhdfh/environments/master/entries?access_token=lpW7spZ1knrCfM4NsWxxPhXKG2oU8FdGctTx4T46yUE&content_type=triviaQ"
+$.ajax(URL)
+.then((data) => {
+  console.log(data)
+})
