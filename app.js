66c4c98b0e83ed2841d031f37ad0aedb4f7da919
Athena Serene <aserenehoosang@gmail.com>
2022-10-25 22:54:42 -0700

    connected api

---
+++ app.js
@@ -0,0 +1,5 @@
+// https://cdn.contentful.com
+// Space ID: j5z000zxhdfh
+// access token: lpW7spZ1knrCfM4NsWxxPhXKG2oU8FdGctTx4T46yUE
+
+//spaces/{space_id}/environments/{environment_id}/entries?access_token={access_token}&content_type={content_type}
