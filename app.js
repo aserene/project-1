b95883a9ba41027d56c09c58176ba87b80677da5
Athena Serene <113308822+aserene@users.noreply.github.com>
2022-11-02 19:07:05 -0700

    updated app.js (removed key)

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// https://cdn.contentful.com
-// Space ID: j5z000zxhdfh
-// access token: lpW7spZ1knrCfM4NsWxxPhXKG2oU8FdGctTx4T46yUE
 
-//spaces/{space_id}/environments/{environment_id}/entries?access_token={access_token}&content_type={content_type}
 const state = {
     player1: 1,
     player2: 1,
